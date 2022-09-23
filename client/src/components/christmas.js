89e8ad606366b973d87e72f4c745f2eed02b0c93
Lisa <lb.broadhead@gmail.com>
2022-09-23 08:56:58 -0600

    christmas and traditions component

--- client/src/components/christmas.js
+++ client/src/components/christmas.js
@@ -4,6 +4,12 @@ const Christmas = () => {
     return(
         <div>
             <h1>Hello Christmas!</h1>
+            <p>Traiditions are a vital part of Christmas!</p>
+            <ul>
+                <li>Christmas Cookies</li>
+                <li>Coraling</li>
+                <li>Sleding</li>
+            </ul>
         </div>
     )
 }; 
---
+++ client/src/components/traditions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Traditions = () => {
+    return(
+        <div>
+            <h1>Traditions</h1>
+            <p>These are some christmas traditions</p>
+        </div>
+    )
+};
+
+export default Traditions;
