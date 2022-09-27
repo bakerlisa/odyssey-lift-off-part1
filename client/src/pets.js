213a1660f1feb325a8d95719e56cd39c7763a7ca
Lisa <lb.broadhead@gmail.com>
2022-09-27 12:25:39 -0600

    pets

--- client/src/pets.js
+++ client/src/pets.js
@@ -3,9 +3,17 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Pests</title>
+    <title>Pets</title>
 </head>
 <body>
+    <h1>Tops pets for the holidays</h1>
+    <ul>
+        <li>dogs</li>
+        <li>cats</li>
+        <li>rat</li>
+        <li>bird</li>
+        <li>fish</li>
+    </ul>
     
 </body>
 </html>
